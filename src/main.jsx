b48f0696bb7568b7466967012db5d60d2fc172fe
Viktor Kovács <ko.vi1996@hotmail.com>
2024-01-11 13:15:38 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { store } from "./store";
+
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
